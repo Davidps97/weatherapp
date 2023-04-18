31ea3c285cd06f2c911e51faef5cea84f9509576
David Perez <davidperezsantana1997@gmail.com>
2023-04-18 16:34:11 +0100

    Second commit

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import './Home.css';
+import Navbar from '../../Components/Header/Navbar';
+import React from 'react';
+
+function Home() {
+  return (
+    <div >
+      <Navbar />
+    </div>
+  );
+}
+
+export default Home;

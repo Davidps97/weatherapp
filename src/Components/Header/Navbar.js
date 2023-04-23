c2c96b364ed1f32bd04ab0d5a291f7b981d2bf1f
David <davidperezsantana1997@gmail.com>
2023-04-23 21:43:44 +0100

    Fourth commit

--- src/Components/Header/Navbar.js
+++ src/Components/Header/Navbar.js
@@ -1,17 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 
 function Navbar() {
+  const [language, setLanguage] = useState("es"); // Inicialmente el idioma es español
+
+  const changeLanguage = (event) => {
+    setLanguage(event.target.value); // Actualiza el idioma cuando se cambia la selección
+  };
+
+  const getCurrentDate = () => {
+    const date = new Date();
+    const options = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+    return date.toLocaleDateString(language, options);
+  };
+
   return (
     <div className="main-header">
-      <div className="logo-header-box"></div>
+      <div className="logo-header-box">
+        <img src={process.env.PUBLIC_URL + "/IconMeteo.jpeg"} alt="MeteoIcon" />
+      </div>
 
-      <div className="search-header-box"></div>
+      <div className="search-header-box">
+        <input className="search" />
+      </div>
 
       <div className="info-lang-header-box">
-        <div className="date-header"></div>
+        <div className="date-header">{getCurrentDate()}</div>
 
-        <div className="lang-header"></div>
+        <div className="lang-header">
+          <label htmlFor="language-select">Elegir idioma:</label>
+          <select
+            id="language-select"
+            value={language}
+            onChange={changeLanguage}
+          >
+            <option value="es">Español</option>
+            <option value="en">Inglés</option>
+          </select>
+        </div>
       </div>
     </div>
   );

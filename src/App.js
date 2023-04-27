b3ee71a12a02521aa4bfe9412e4dc6d877fb5f46
David <davidperezsantana1997@gmail.com>
2023-04-27 01:14:17 +0100

    Navbar media screen

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
+import Map from './Pages/Map/Map';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path='/' element = {<Home/>}/> 
           <Route path='/home' element = {<Home/>}/> 
+          <Route path='/map' element = {<Map/>}/> 
         </Routes>
       </BrowserRouter>
     </div>
